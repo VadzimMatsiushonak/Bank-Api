64865eab5777bcc82ad79ddfe0b51ba2e381b0b0
Vadzim Matsiushonak <vadzimmatsiushonak@gmail.com>
2022-11-16 02:21:19 +0300

    (feat) [BA-1] Setup project structure - #4gqqj5c

---
+++ src/main/java/by/vadzimmatsiushonak/bank/api/controller/HelloRestController.java
@@ -0,0 +1,14 @@
+package by.vadzimmatsiushonak.bank.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+
+}
