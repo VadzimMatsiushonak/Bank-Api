5e971ad8ef10c55b1bf4b0402f7fe7dc6f5478b6
Vadzim Matsiushonak <118316719+VadzimMatsiushonak@users.noreply.github.com>
2022-12-11 22:52:50 +0300

    (feat) [BA-23] Add status for BankPayment (#12)

--- src/main/java/by/vadzimmatsiushonak/bank/api/model/entity/BankPayment.java
+++ src/main/java/by/vadzimmatsiushonak/bank/api/model/entity/BankPayment.java
@@ -1,8 +1,11 @@
 package by.vadzimmatsiushonak.bank.api.model.entity;
 
 import by.vadzimmatsiushonak.bank.api.model.entity.base.BaseEntity;
+import by.vadzimmatsiushonak.bank.api.model.entity.base.PaymentStatus;
 import java.math.BigDecimal;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import lombok.Getter;
@@ -16,6 +19,9 @@ public class BankPayment extends BaseEntity {
     private BigDecimal amount;
     private String currency;
     private Long recipientBankAccountId;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
 
     @ManyToOne
     @JoinColumn(name = "bankAccount_id", nullable = false)
