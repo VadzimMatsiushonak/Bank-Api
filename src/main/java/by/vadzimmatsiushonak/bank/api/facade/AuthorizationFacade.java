c267087b6415c93839eb7c6cdece275424cdfe09
Vadzim Matsiushonak <vadzimmatsiushonak@gmail.com>
2023-05-15 00:13:52 +0300

    (feat) [BA-41] Implemented registration flow

--- src/main/java/by/vadzimmatsiushonak/bank/api/facade/AuthorizationFacade.java
+++ src/main/java/by/vadzimmatsiushonak/bank/api/facade/AuthorizationFacade.java
@@ -2,10 +2,11 @@ package by.vadzimmatsiushonak.bank.api.facade;
 
 import by.vadzimmatsiushonak.bank.api.model.entity.Customer;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 public interface AuthorizationFacade {
 
-    String registerCustomer(Customer customer);
+    String registerCustomer(@NotNull Customer customer);
 
     Boolean confirmCustomer(@NotBlank String key, @NotBlank String code);
 
