0e68e3d471902d9a77946298fae3f0b191ac2454
Vadzim Matsiushonak <vadzimmatsiushonak@gmail.com>
2023-06-26 22:36:02 +0300

    (feat) [BA-71] Update InitiatePayment flow - Retrieving phoneNumber from authorization - Using Iban instead of IDs

--- src/main/java/by/vadzimmatsiushonak/bank/api/controller/BankPaymentController.java
+++ src/main/java/by/vadzimmatsiushonak/bank/api/controller/BankPaymentController.java
@@ -1,12 +1,5 @@
 package by.vadzimmatsiushonak.bank.api.controller;
 
-
-import static by.vadzimmatsiushonak.bank.api.constant.SwaggerConstant.EMPTY_DESCRIPTION;
-import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
-import static java.net.HttpURLConnection.HTTP_CREATED;
-import static org.springframework.http.HttpStatus.CREATED;
-import static org.springframework.http.HttpStatus.OK;
-
 import by.vadzimmatsiushonak.bank.api.facade.PaymentFacade;
 import by.vadzimmatsiushonak.bank.api.mapper.BankPaymentMapper;
 import by.vadzimmatsiushonak.bank.api.mapper.BankPaymentStatusMapper;
@@ -20,20 +13,19 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-
-import java.security.Principal;
-import java.util.List;
-import javax.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+import static by.vadzimmatsiushonak.bank.api.constant.SwaggerConstant.EMPTY_DESCRIPTION;
+import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
+import static java.net.HttpURLConnection.HTTP_CREATED;
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.OK;
 
 @Api(tags = "Payment", description = EMPTY_DESCRIPTION)
 @AllArgsConstructor
@@ -51,31 +43,35 @@ public class BankPaymentController {
     @GetMapping("/{id}")
     public ResponseEntity<BankPaymentDtoRelations> findById(@PathVariable Long id) {
         return ResponseEntity.status(OK)
-            .body(bankPaymentMapper.toDtoRelations(bankPaymentService.findById(id).orElse(null)));
+                .body(bankPaymentMapper.toDtoRelations(
+                        bankPaymentService.findById(id).orElse(null)));
     }
 
     @ApiOperation("Get List of Payments with property relations")
     @GetMapping
     public ResponseEntity<List<BankPaymentDtoRelations>> findAll() {
         return ResponseEntity.status(OK)
-            .body(bankPaymentMapper.toListDtoRelations(bankPaymentService.findAll()));
+                .body(bankPaymentMapper.toListDtoRelations(bankPaymentService.findAll()));
     }
 
     @ApiOperation("Add the Payment to the Api database")
     @ResponseStatus(CREATED)
     @PostMapping
-    public ResponseEntity<BankPayment> create(@Valid @RequestBody BankPaymentRequestDto bankPaymentRequestDto) {
+    public ResponseEntity<BankPayment> create(
+            @Valid @RequestBody BankPaymentRequestDto bankPaymentRequestDto) {
         return ResponseEntity.status(CREATED)
-            .body(bankPaymentService.create(bankPaymentMapper.toEntity(bankPaymentRequestDto)));
+                .body(bankPaymentService.create(bankPaymentMapper.toEntity(bankPaymentRequestDto)));
     }
 
     @ApiOperation("Initiates a payment between 2 accounts")
-    @ApiResponses({@ApiResponse(code = HTTP_CREATED, message = "Provides created Payment from database"),
-        @ApiResponse(code = HTTP_BAD_REQUEST, message = "Invalid arguments provided")})
+    @ApiResponses({
+            @ApiResponse(code = HTTP_CREATED, message = "Provides created Payment from database"),
+            @ApiResponse(code = HTTP_BAD_REQUEST, message = "Invalid arguments provided")})
     @ResponseStatus(CREATED)
     @PostMapping("/initiatePayment")
     public ResponseEntity<BankPaymentStatusResponse> initiatePayment(
-            @Valid @RequestBody InitiatePaymentRequest initiatePaymentRequest, Authentication authentication) {
+            @Valid @RequestBody InitiatePaymentRequest initiatePaymentRequest,
+            Authentication authentication) {
         BankPayment result = paymentFacade.initiatePayment(authentication.getName(),
                 initiatePaymentRequest);
         return ResponseEntity.status(CREATED).body(bankPaymentStatusMapper.toDto(result));
