821a5897a3d62a64bb1c103bf90eb76545899fef
Kihtenko Olga <kiok.ko1811@gmail.com>
2023-07-27 14:15:46 +0300

    (feat) [BA-31] Add initiate Payment confirmation process

--- src/main/java/by/vadzimmatsiushonak/bank/api/constant/MetadataConstants.java
+++ src/main/java/by/vadzimmatsiushonak/bank/api/constant/MetadataConstants.java
@@ -2,8 +2,7 @@ package by.vadzimmatsiushonak.bank.api.constant;
 
 public class MetadataConstants {
 
-    public final static String KEY_USER_ID = "userId";
-    public final static String KEY_LOGIN = "userLogin";
-    public final static String KEY_PAYMENT_ID = "paymentId";
+    public final static String ID = "id";
+    public final static String LOGIN = "login";
 
 }
